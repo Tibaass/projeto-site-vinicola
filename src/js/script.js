be1f019638d423edc9b64534fbb4a9d883546085
ssnkz <pedrohpc06@gmail.com>
2026-09-22 22:27:10 -0300

    feat: Novas Paginas Adicionadas e Uma funalão no Js:
    - Pagina de Reservar Vinho
    - Pagina de Historia da Vinicola
    - JS: Botão de reservar gerar um alert pro usuario

---
+++ src/js/script.js
@@ -0,0 +1,2 @@
+const btn_reservar = document.getElementById("reservar");
+btn_reservar.addEventListener("click", function() {alert("Reserva realizada com sucesso! Entraremos em contato em breve.");});
